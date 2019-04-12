96c38a595e3ef317d5d67aa46ac89024a0171ff2
Tri Dang <TriDangContact@gmail.com>
2019-04-12 02:22:31 -0700

    Allow user to pick image from gallery. Added progress bars.

--- app/src/main/java/com/android/instapost/CreatePostActivity.java
+++ app/src/main/java/com/android/instapost/CreatePostActivity.java
@@ -1,11 +1,17 @@
 package com.android.instapost;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
@@ -14,10 +20,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
@@ -27,11 +37,17 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -44,7 +60,10 @@ public class CreatePostActivity extends AppCompatActivity {
     private static final String TAG_DB_PATH = "tag";
     private static final String POST_DB_PATH = "post";
     private static final String TAG_DB_ORDER_BY = "mHashtag";
-    private static final int REQUEST_IMAGE = 0;
+    private static final int REQUEST_CAMERA = 0;
+    private static final int REQUEST_GALLERY = 1;
+    private static final String DOWNLOAD_DIR = Environment.getExternalStoragePublicDirectory
+            (Environment.DIRECTORY_DOWNLOADS).getPath();
 
     private FirebaseDatabase mDatabase;
     private ImageView mImageView;
@@ -54,6 +73,8 @@ public class CreatePostActivity extends AppCompatActivity {
     private AppCompatButton mCreatePostButton;
     private TextView mCancelLink;
     private String mCurrentPhotoPath = "";
+    private Uri mPhotoUri;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,11 +89,13 @@ public class CreatePostActivity extends AppCompatActivity {
         mHashtagText = (EditText) findViewById(R.id.createpost_input_hashtag);
         mCreatePostButton = (AppCompatButton) findViewById(R.id.createpost_btn_post);
         mCancelLink = (TextView) findViewById(R.id.createpost_link_cancel);
+        mProgressBar = (ProgressBar) findViewById(R.id.createpost_loading);
 
         mCaptureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onLaunchCamera();
+                showPictureDialog();
+                //launchCamera();
             }
         });
 
@@ -80,10 +103,15 @@ public class CreatePostActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (validate()) {
-                    addPhotoToDB(mCurrentPhotoPath);
+                    mProgressBar.setVisibility(ProgressBar.VISIBLE);
+                    try {
+                        downsizeImage(mPhotoUri);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     addTagToDB(mHashtagText.getText().toString());
-                    setResult(RESULT_OK);
-                    finish();
+                    addPhotoToDB(mPhotoUri);
+
                 }
                 else {
                     Toast.makeText(CreatePostActivity.this, getString(R.string.no_photo),
@@ -107,8 +135,39 @@ public class CreatePostActivity extends AppCompatActivity {
     -------------------------------------------------------------------------------
     */
 
+    private void showPictureDialog(){
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+        pictureDialog.setTitle("Select Action");
+        String[] pictureDialogItems = {
+                "Select photo from gallery",
+                "Capture photo from camera" };
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                choosePhotoFromGallery();
+                                break;
+                            case 1:
+                                launchCamera();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
+    }
+
+    public void choosePhotoFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_PICK);
+        startActivityForResult(Intent.createChooser(intent, "Select Image"), REQUEST_GALLERY);
+
+    }
+
     // use function from Android Document to take a photo
-    public void onLaunchCamera() {
+    public void launchCamera() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -126,7 +185,7 @@ public class CreatePostActivity extends AppCompatActivity {
                         "com.android.instapost.fileprovider",
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE);
+                startActivityForResult(takePictureIntent, REQUEST_CAMERA);
             }
         }
     }
@@ -149,40 +208,60 @@ public class CreatePostActivity extends AppCompatActivity {
 
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = image.getAbsolutePath();
+        mPhotoUri = Uri.fromFile(image);
         return image;
     }
 
     // once the user is done taking a photo, we load it into the thumbnail
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE) {
+        if (requestCode == REQUEST_CAMERA) {
             if (resultCode == RESULT_OK) {
-                loadPhotoToThumbnail(mCurrentPhotoPath);
-                galleryAddPic(mCurrentPhotoPath);
+                loadPhotoToThumbnail(mPhotoUri);
             }
-            if (resultCode == RESULT_CANCELED) {
+            else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(CreatePostActivity.this, getString(R.string.cancelled_photo),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if (requestCode == REQUEST_GALLERY) {
+            if (resultCode == RESULT_OK) {
+                mPhotoUri = data.getData();
+                loadPhotoToThumbnail(mPhotoUri);
+            }
+            else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(CreatePostActivity.this, getString(R.string.cancelled_gallery),
                         Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private void loadPhotoToThumbnail(String imageUrl) {
+    private void loadPhotoToThumbnail(Uri uri) {
         Glide
             .with(this)
-            .load(imageUrl)
+            .load(uri)
             .override(mImageView.getWidth(), mImageView.getHeight())
             .into(mImageView);
     }
 
-    // add the photo to the device's default gallery app
-    private void galleryAddPic(String imageUrl) {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File file = new File(imageUrl);
-        Uri contentUri = Uri.fromFile(file);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
+    // three step process to convert URI to bitmap, then back to URI
+    private Uri downsizeImage(Uri uri) throws IOException{
+        Bitmap image = createBitmap(uri);
+        return getImageUri(image);
     }
+
+    private Bitmap createBitmap(Uri uri) throws IOException{
+        return MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+    }
+
+    private Uri getImageUri(Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 50, bytes);
+        String path = MediaStore.Images.Media.insertImage(this.getContentResolver(), inImage,
+                "Title", null);
+        return Uri.parse(path);
+    }
+
     /*
     -------------------------------------------------------------------------------
     END OF: ASK USER TO TAKE A PHOTO PUT IT INTO THE THUMBNAIL
@@ -194,9 +273,8 @@ public class CreatePostActivity extends AppCompatActivity {
         boolean valid = true;
         // TODO: implement method to validate the post before posting it
         String hashtag = mHashtagText.getText().toString().trim();
-        String photoPath = mCurrentPhotoPath;
 
-        if (photoPath.isEmpty()) {
+        if (mCurrentPhotoPath.isEmpty() && mPhotoUri == null) {
             valid = false;
         }
 
@@ -231,19 +309,23 @@ public class CreatePostActivity extends AppCompatActivity {
 
     // 1. before we can create a post, we need to upload the photo to db and get db path so that we
     // can store it in the Post
-    private void addPhotoToDB(String imageUrl) {
+    private void addPhotoToDB(Uri picUri) {
         // TODO: add the photo to Firebase Storage
-        File file = new File(imageUrl);
-        Uri picUri = Uri.fromFile(file);
-        final String cloudFilePath = getCurrentUserName() + picUri.getLastPathSegment();
+        String uid = UUID.randomUUID().toString();
+        final String cloudFilePath = getCurrentUserName() + uid;
+        final ProgressDialog progressDialog = new ProgressDialog(this);
 
         StorageReference storageRef = FirebaseStorage.getInstance().getReference();
         StorageReference uploadeRef = storageRef.child(cloudFilePath);
+        StorageMetadata metadata = new StorageMetadata.Builder()
+                .setContentType("image/jpg")
+                .build();
 
-        uploadeRef.putFile(picUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>(){
+        uploadeRef.putFile(picUri, metadata).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>(){
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot){
                 addPostToDB(createPost(cloudFilePath));
+                mProgressBar.setVisibility(ProgressBar.INVISIBLE);
             }
         }).addOnFailureListener(new OnFailureListener(){
             public void onFailure(@NonNull Exception exception){
@@ -274,6 +356,8 @@ public class CreatePostActivity extends AppCompatActivity {
         postTable.child(uid).setValue(post);
         Toast.makeText(CreatePostActivity.this, "Post created",
                 Toast.LENGTH_SHORT).show();
+        setResult(RESULT_OK);
+        finish();
     }
 
     // check if a hashtag already exists in firebase; if not, add it
--- app/src/main/java/com/android/instapost/PostRecyclerAdapter.java
+++ app/src/main/java/com/android/instapost/PostRecyclerAdapter.java
@@ -1,6 +1,8 @@
 package com.android.instapost;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,9 +10,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -52,7 +60,7 @@ public class PostRecyclerAdapter extends RecyclerView.Adapter<PostRecyclerAdapte
         holder.mIdView.setText(mValues.get(position).mUsername);
         holder.mCaptionView.setText(mValues.get(position).mCaption);
         holder.mHashtagView.setText(mValues.get(position).mHashtag);
-        loadImage(holder.mImageView, mValues.get(position).mImagePath);
+        loadImage(holder.mImageView, mValues.get(position).mImagePath, holder.mProgressBar);
 
 //        holder.mView.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -107,13 +115,26 @@ public class PostRecyclerAdapter extends RecyclerView.Adapter<PostRecyclerAdapte
     }
 
     // get the image from firebase storage and load it using Glide library
-    private void loadImage(ImageView imageView, String filePath) {
+    private void loadImage(ImageView imageView, String filePath, ProgressBar progress) {
+        final ProgressBar progressBar = progress;
         StorageReference storageReference = FirebaseStorage.getInstance().getReference().child(filePath);
         Glide
             .with(mContext)
             .load(storageReference)
             .centerCrop()
-            .placeholder(R.drawable.loading)
+            .listener(new RequestListener<Drawable>() {
+                @Override
+                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                    progressBar.setVisibility(View.GONE);
+                    return false;
+                }
+                @Override
+                public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                    progressBar.setVisibility(View.GONE);
+                    return false;
+                }
+            })
+            .transition(DrawableTransitionOptions.withCrossFade())
             .into(imageView);
     }
 
@@ -124,6 +145,7 @@ public class PostRecyclerAdapter extends RecyclerView.Adapter<PostRecyclerAdapte
         public final TextView mHashtagView;
         public final ImageView mImageView;
         public final ImageView mOptionsView;
+        public final ProgressBar mProgressBar;
         // TODO: replace with your data class object
         public Post mItem;
 
@@ -135,6 +157,7 @@ public class PostRecyclerAdapter extends RecyclerView.Adapter<PostRecyclerAdapte
             mHashtagView = (TextView) view.findViewById(R.id.fm_post_hashtag);
             mImageView = (ImageView) view.findViewById(R.id.fm_post_image);
             mOptionsView = (ImageView) view.findViewById(R.id.fm_post_option);
+            mProgressBar = (ProgressBar) view.findViewById(R.id.fm_post_progress);
         }
 
         @Override
